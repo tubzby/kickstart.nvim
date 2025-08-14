ee6e9f6663bc75d718bef5dc5bf7b1f19e334bff
tubzby <tubzby@gmail.com>
2025-08-14 09:02:24 +0800

    feat oil: esapce to close window

--- lua/custom/plugins/oil.lua
+++ lua/custom/plugins/oil.lua
@@ -9,6 +9,15 @@ return {
   -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
   keys = {
     { '-', ':Oil<CR>', desc = 'Oil', silent = true },
+    {
+      '<Esc>',
+      function()
+        if vim.bo.filetype == 'oil' then
+          require('oil').close()
+        end
+      end,
+      desc = 'Close float',
+    },
   },
   lazy = true,
 }
